83f0c2c62f39b769c1ff673ea72c5dd11e6f7e4e
Fabio Papa <fabtheman@gmail.com>
2019-09-25 19:55:49 -0700

    Implement fletcher as follows:
    - Get a URL and write it to a file
    - If file exists, ask before overwriting it
    - Report the size of the file written

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,29 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 const request = require('request');
 const fs = require('fs');
 
 const fetch = (url, fileName) => {
-  let htmlData;
-
   request(url, (error, response, body) => {
-    htmlData = body;
-    fs.writeFile(fileName, htmlData, () => {});
+    fs.open(fileName, (err, fd) => {
+      if (fd) {
+        rl.question('File exists. Do you want to overwrite it?', (ans) => {
+          if (ans === 'y') {
+            fs.write(fd, body, () => {});
+          }
+          rl.close();
+        });
+      } else {
+        fs.writeFile(fileName, body, () => {});
+        rl.close();
+      }
+    });
     console.log(`Downloaded and saved ${response.headers['content-length']} bytes to ${fileName}`);
   });
+
   return;
 };
 
